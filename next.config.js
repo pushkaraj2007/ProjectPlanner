fd28e13861c3343a2afb379b927f3d725a14a012
Pushkaraj Kulkarni <pushkarajkulkarni07@gmail.com>
2023-07-29 15:06:29 +0530

    Add google images domain to next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
+}
