c863ffc4b06b07a66b296dae4e79fb2e2d12c346
Pushkaraj Kulkarni <pushkarajkulkarni07@gmail.com>
2023-08-02 00:33:10 +0530

    Make background light

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body className={`{inter.className}`}>
+            <body className={`{inter.className} bg-gradient-to-b from-gray-50 to-gray-100 `}>
                 <Providers>
                     <Header />
                     {children}
