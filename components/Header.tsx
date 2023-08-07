736654ea0374ca6a47ed71cbaf75a2f26cd8201b
Pushkaraj Kulkarni <pushkarajkulkarni07@gmail.com>
2023-08-08 01:07:41 +0530

    Some minor change

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,24 +1,18 @@
-"use client";
-
 import { NextPage } from 'next'
-import { FaMoon, FaSun } from 'react-icons/fa'
 import Link from 'next/link'
-import { useSession, signIn } from 'next-auth/react'
 import SigninButton from './SigninButton';
 
 const Header: NextPage = () => {
 
     return (
         <header className='text-gray-600 bg-gray-50 body-font dark:bg-gray-900'>
-            <div className='container flex flex-wrap items-center justify-between p-5 mx-auto md:flex-row'>
-                <div className='topNavbar flex flex-row items-center'>
-                    <div className='flex flex-row items-center'>
+            <div className='flex flex-wrap items-center justify-between p-5 md:flex-row'>
+                <div className='flex flex-row items-center'>
                         <Link className='cursor-pointer' href='/'>
                             <div className='flex items-center font-medium text-gray-900 title-font dark:text-gray-300 logoDiv'>
                                 <span className='ml-3 text-2xl font-bold text-blue-600'>ProjectPlanner</span>
                             </div>
                         </Link>
-                    </div>
                 </div>
 
                 <SigninButton />
