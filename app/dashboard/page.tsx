a8cd59fe57233a93b176afea64d54b8e233e48a0
Pushkaraj Kulkarni <pushkarajkulkarni07@gmail.com>
2023-08-09 00:47:16 +0530

    Improve the design of the cards

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,49 +1,43 @@
-import React from 'react'
-import { BsPlusCircle } from 'react-icons/bs'
-import Link from 'next/link'
+import React from 'react';
+import { BsPlusCircle } from 'react-icons/bs';
+import Link from 'next/link';
 
-const Dashboard = () => {
-
+const Dashboard: React.FC = () => {
     return (
-        <div className=' min-h-screen'>
-            <div className=' text-2xl font-bold flex justify-center'>
-                Your creations ✨
+        <div className="min-h-screen py-8">
+            <div className="text-3xl font-bold text-center mb-8 text-gray-800">
+                Your Creations ✨
             </div>
 
-            <div className='flex flex-wrap justify-center mt-12'>
-                <div className='w-[400px] k p-4'>
-                    <Link href={'/create-new'}>
-                        <div className='flex h-[300px] flex-col items-center justify-center border dark:border-gray-600 border-gray-300 p-6 rounded-lg duration-500 hover:transform hover:-translate-y-1 transition cursor-pointer'>
-                            <div className='inline-flex items-center justify-center rounded-full mb-5'>
-                                <BsPlusCircle size={30} />
-                            </div>
-                            <h2 className='text-lg dark:text-gray-300 text-gray-900 font-medium title-font mb-2'>
-                                Create New
-                            </h2>
+            <div className='flex items-center justify-center w-full'>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-4 w-[85%] xl:w-[70%]">
+                    <Link href="/create-new" className="flex flex-col items-center justify-center p-6 border border-gray-300 rounded-lg hover:shadow-lg transition duration-300 transform hover:-translate-y-1 hover:scale-105 cursor-pointer bg-white min-h-[250px]">
+                        <div className="rounded-full bg-blue-500 text-white p-4 mb-4">
+                            <BsPlusCircle size={30} />
                         </div>
+                        <h2 className="text-lg text-gray-800 font-medium text-center">
+                            Create New
+                        </h2>
                     </Link>
-                </div>
-                <div className=' w-[400px] k p-4 hover:transform hover:-translate-y-1 transition duration-500 cursor-pointer'>
-                    <div className='flex h-[300px] flex-col items-center justify-center border dark:border-gray-600 border-gray-300 p-6 rounded-lg transition duration-500'>
-                        <h2 className='text-2xl dark:text-gray-300 text-center text-gray-900 font-medium title-font mb-2'>
+
+                    <div className="flex flex-col items-center justify-center p-6 border border-gray-300 rounded-lg hover:shadow-lg transition duration-300 transform hover:-translate-y-1 hover:scale-105 cursor-pointer bg-white min-h-[250px]">
+                        <h2 className="text-2xl text-gray-800 font-medium text-center">
                             JavaScript
                         </h2>
                     </div>
-                </div>
-                <div className=' w-[400px] k p-4 hover:transform hover:-translate-y-1 transition duration-500 cursor-pointer'>
-                    <div className='flex h-[300px] flex-col items-center justify-center border dark:border-gray-600 border-gray-300 p-6 rounded-lg transition duration-500'>
-                        <div className='inline-flex items-center justify-center rounded-full mb-5'>
+
+                    <div className="flex flex-col items-center justify-center p-6 border border-gray-300 rounded-lg hover:shadow-lg transition duration-300 transform hover:-translate-y-1 hover:scale-105 cursor-pointer bg-white min-h-[250px]">
+                        <div className="rounded-full bg-blue-500 text-white p-4 mb-4">
                             <BsPlusCircle size={30} />
                         </div>
-                        <h2 className='text-lg dark:text-gray-300 text-gray-900 font-medium title-font mb-2'>
+                        <h2 className="text-lg text-gray-800 font-medium text-center">
                             Create New
                         </h2>
                     </div>
                 </div>
-
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Dashboard
+export default Dashboard;
