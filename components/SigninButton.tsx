397381637e18fc40872123b82f818cb7789458c7
Pushkaraj Kulkarni <pushkarajkulkarni07@gmail.com>
2023-08-13 20:38:01 +0530

    Add credits showing button

--- components/SigninButton.tsx
+++ components/SigninButton.tsx
@@ -10,14 +10,19 @@ const SigninButton = () => {
     if (session && session.user) {
         return (
             <div className="flex items-center gap-4 ml-auto">
-                <button onClick={()=> signOut()}>Signout</button>
+                <button className="border border-transparent py-1 px-2 transition duration-300 hover:border-green-500">🔥 5</button>
+                <button onClick={()=> signOut()}>
+                    <Image src={session.user.image as string} width={40} height={40} alt="Profile Image" className="rounded-full" />
+                </button>
             </div>
         );
     }
 
     return (
         <button
-            onClick={() => signIn()}
+            onClick={() => signIn('google', {
+                callbackUrl: 'http://localhost:3000/api/user-signin'
+            })}
             className="text-green-600 text-xl border border-transparent rounded-md py-2 px-4 transition duration-300 hover:border-green-500"
         >
             Login / Signup
