94c7deb9a6b510612aa0d72d6301ca6a02809051
Pushkaraj Kulkarni <pushkarajkulkarni07@gmail.com>
2023-08-13 22:46:36 +0530

    Add database connection and user schema

---
+++ database/users.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    profileImage: {
+        type: String,
+        required: true
+    },
+    tokens: {
+        type: Number,
+        required: true
+    }
+})
+
+const user = mongoose.models.user || mongoose.model('user', userSchema)
+
+export default user
