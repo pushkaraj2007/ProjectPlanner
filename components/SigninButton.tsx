6e3248ae375c96592043be0dbdc98061f363c7a3
Pushkaraj Kulkarni <pushkarajkulkarni07@gmail.com>
2023-08-19 01:35:30 +0530

    Add children elements for Modal

--- components/SigninButton.tsx
+++ components/SigninButton.tsx
@@ -1,19 +1,50 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { useState } from "react";
+import { useContext } from 'react';
+import TokenContext from "@/context/tokens/tokenContext";
 import Image from "next/image";
+import Modal from "./Modal";
+import Dropdown from "./Dropdown";
 
 const SigninButton = () => {
+    const tokens = useContext(TokenContext)
     const { data: session } = useSession();
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
 
     if (session && session.user) {
         return (
             <div className="flex items-center gap-4 ml-auto">
-                <button className="border border-transparent py-1 px-2 transition duration-300 hover:border-green-500">🔥 5</button>
-                <button onClick={()=> signOut()}>
-                    <Image src={session.user.image as string} width={40} height={40} alt="Profile Image" className="rounded-full" />
-                </button>
+                <button className="border border-transparent py-1 px-2 transition duration-300 hover:border-green-500" onClick={openModal}>🔥 {tokens.tokenCount}</button>
+
+                <div>
+                    <Modal isOpen={isModalOpen} onClose={closeModal}>
+                        <h1 className="text-xl font-semibold mb-2">Modal Content</h1>
+                        <p className="mb-4">Do you have any token related code? Apply it here:</p>
+                        <div className="flex flex-col mb-4">
+                            <input
+                                type="text"
+                                placeholder="Your token code..."
+                                className="bg-gray-100 outline-none text-black text-lg border border-gray-300 py-2 px-4 rounded-l-md w-full focus:ring-2 focus:ring-blue-400 mb-4"
+                            />
+                            <button className="bg-blue-500 text-white py-2 px-4 rounded-r-md hover:bg-blue-600 transition duration-300 focus:ring-2 focus:ring-blue-400">Apply ✅</button>
+                        </div>
+                        <p>If you don't have one you can contact me on <a href="https://twitter.com/pushkaraj2007">X (Twitter)</a></p>
+                        <p>or at <a href="mailto:contactpushkaraj@gmail.com">conatcpushkaraj@gmail.com</a> and I'll help you t get some extra tokens</p>
+                    </Modal>
+                </div>
+                <Dropdown />
             </div>
         );
     }
