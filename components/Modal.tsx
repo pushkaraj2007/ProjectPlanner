2b573e6872d195fb31a43c9ced7d802a5990ac88
Pushkaraj Kulkarni <pushkarajkulkarni07@gmail.com>
2023-08-20 23:47:54 +0530

    Improve Modal design

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -46,7 +46,7 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
         <div className="fixed inset-0 flex items-center justify-center z-50 bg-opacity-50 bg-gray-900">
             <div
                 ref={modalRef}
-                className="bg-white p-6 rounded-lg shadow-lg transform transition-transform ease-in-out duration-300 scale-100 slide-in"
+                className="bg-white p-6 rounded-lg shadow-lg transform transition-transform ease-in-out duration-300 scale-100 slide-in w-[370px] md:w-[500px]"
             >
                 <button
                     className="absolute top-2 right-2 text-gray-500"
