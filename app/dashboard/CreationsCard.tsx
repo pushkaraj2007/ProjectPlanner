7e1bf6e1d903b7e45c8ed15a395dc9d019dba6c3
Pushkaraj Kulkarni <pushkarajkulkarni07@gmail.com>
2023-08-22 22:45:35 +0530

    Make back button workable

--- app/dashboard/CreationsCard.tsx
+++ app/dashboard/CreationsCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Key, useContext, useEffect, useRef, useState } from 'react';
+import { Key, useEffect, useRef, useState } from 'react';
 import User from '@/database/users'; // Import your Mongoose User model here
 import { useSession } from 'next-auth/react';
 import Link from 'next/link';
@@ -20,11 +20,11 @@ interface Creation {
 
 const UserCard: React.FC = () => {
     const [creations, setCreations] = useState<Array<Creation>>([]);
-    const [isLoading, setIsLoading] = useState(true)
-    const [projectsIdeasArray, setProjectsIdeasArray] = useState([])
-    const technologiesSectionRef = useRef<HTMLInputElement>(null)
-    const GeneratedProjectsRef = useRef<HTMLInputElement>(null)
-
+    const [isLoading, setIsLoading] = useState(true);
+    const [projectsIdeasArray, setProjectsIdeasArray] = useState([]);
+    const [isTechnologiesVisible, setIsTechnologiesVisible] = useState(false);
+    const technologiesSectionRef = useRef<HTMLInputElement>(null);
+    const GeneratedProjectsRef = useRef<HTMLInputElement>(null);
 
     useEffect(() => {
         const fetchUsers = async () => {
@@ -49,64 +49,67 @@ const UserCard: React.FC = () => {
     }, []);
 
     const generateIdeas = (projectsIdeas: any) => {
-        console.log('hello')
-        setProjectsIdeasArray(projectsIdeas)
-        // Implement your logic for generating ideas here
-        const technologiesSection = technologiesSectionRef.current as any
-        const projectsSection = GeneratedProjectsRef.current as any
+        const technologiesSection = technologiesSectionRef.current;
 
-        technologiesSection.id = 'name-section'
+        technologiesSection?.classList.add('hidden');
+        setIsTechnologiesVisible(true);
+        setProjectsIdeasArray(projectsIdeas);
+    };
 
-        technologiesSection.addEventListener('animationend', () => {
-            projectsSection.classList.remove('hidden')
-            projectsSection.classList.add('block')
-            technologiesSection.classList.add('hidden')
-        })
+    const goBack = () => {
+        const technologiesSection = technologiesSectionRef.current;
+        const projectsSection = GeneratedProjectsRef.current;
+
+        if (technologiesSection && projectsSection) {
+            technologiesSection.classList.add('slide-in-back');
+            projectsSection.classList.remove('hidden');
+            technologiesSection?.classList.remove('hidden');
+            setIsTechnologiesVisible(false);
+        }
     };
 
     return (
         <>
-            {isLoading ?
-
-
+            {isLoading ? (
                 <div>
                     Loading...
                 </div>
-
-                :
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-4 w-[85%] xl:w-[70%] transform duration-500" ref={technologiesSectionRef}>
-                    <Link href="/create-new" className="flex flex-col items-center justify-center p-6 border border-gray-300 rounded-lg hover:shadow-lg transition duration-300 transform hover:-translate-y-1 hover:scale-105 cursor-pointer bg-white min-h-[250px]">
-                        <div className="rounded-full bg-blue-500 text-white p-4 mb-4">
-                            <BsPlusCircle size={30} />
-                        </div>
-                        <h2 className="text-lg text-gray-800 font-medium text-center">
-                            Create New
-                        </h2>
-                    </Link>
-
-                    {creations.map((creation) => (
-                        <div
-                            key={creation.name}
-                            className="flex flex-col items-center justify-center p-6 border border-gray-300 rounded-lg hover:shadow-lg transition duration-300 transform hover:-translate-y-1 hover:scale-105 cursor-pointer bg-white min-h-[250px]"
-                            onClick={() => generateIdeas(creation.projectsIdeas)}
-                        >
-                            <h2 className="text-2xl text-gray-800 font-medium text-center">
-                                {creation.name}
+            ) : (
+                <>
+                    <div className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-4 w-[85%] xl:w-[70%] transform duration-500 ${isTechnologiesVisible ? 'slide-out hidden' : ''}`} ref={technologiesSectionRef}>
+                        <Link href="/create-new" className="flex flex-col items-center justify-center p-6 border border-gray-300 rounded-lg hover:shadow-lg transition duration-300 transform hover:-translate-y-1 hover:scale-105 cursor-pointer bg-white min-h-[250px]">
+                            <div className="rounded-full bg-blue-500 text-white p-4 mb-4">
+                                <BsPlusCircle size={30} />
+                            </div>
+                            <h2 className="text-lg text-gray-800 font-medium text-center">
+                                Create New
                             </h2>
+                        </Link>
+                        {creations.map((creation) => (
+                            <div
+                                key={creation.name}
+                                className="flex flex-col items-center justify-center p-6 border border-gray-300 rounded-lg hover:shadow-lg transition duration-300 transform hover:-translate-y-1 hover:scale-105 cursor-pointer bg-white min-h-[250px]"
+                                onClick={() => generateIdeas(creation.projectsIdeas)}
+                            >
+                                <h2 className="text-2xl text-gray-800 font-medium text-center">
+                                    {creation.name}
+                                </h2>
+                            </div>
+                        ))}
+                    </div>
+                    {isTechnologiesVisible && (
+                        <div className="container min-h-screen" id='technologies-section' ref={GeneratedProjectsRef}>
+                            <div className="flex flex-col slide-out" onAnimationEnd={goBack}>
+                                <div className="flex cursor-pointer items-center" onClick={goBack}>
+                                    <IoChevronBackOutline size={24} />
+                                    <p className='ml-2'>Back</p>
+                                </div>
+                                <ProjectGrid projects={projectsIdeasArray} />
+                            </div>
                         </div>
-                    ))}
-                </div>
-            }
-
-            <div className="hidden container min-h-screen flex flex-col" id='technologies-section' ref={GeneratedProjectsRef}>
-                <div>
-                    <button className="flex cursor-pointer items-center">
-                        <IoChevronBackOutline size={24} />
-                        <p className='ml-2'>Back</p>
-                    </button>
-                </div>
-                <ProjectGrid projects={projectsIdeasArray} />
-            </div>
+                    )}
+                </>
+            )}
         </>
     );
 };
