5ae4c17b4e4a5e1c2f27ba4e8c643ea25247716c
Pushkaraj Kulkarni <pushkarajkulkarni07@gmail.com>
2023-08-29 23:05:02 +0530

    Delete db folder

--- database/connection.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const connectToMongo = async ()=>{
-    await mongoose.connect(`${process.env.MONGO_URI}`)
-    console.log('Database Connected')
-}
-
-export default connectToMongo
