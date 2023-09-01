4fa3efe238de408fb6c6c50038c7be98b5e161c0
Pushkaraj Kulkarni <pushkarajkulkarni07@gmail.com>
2023-09-02 00:54:33 +0530

    Minor changes to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ const Header: NextPage = () => {
 
     return (
         <header className='text-gray-600 bg-gray-50 body-font dark:bg-gray-900'>
-            <div className='flex flex-wrap items-center justify-between p-5 md:flex-row'>
+            <div className='flex flex-wrap items-center justify-between p-7 px-24 md:flex-row'>
                 <div className='flex flex-row items-center'>
                         <Link className='cursor-pointer' href='/'>
                             <div className='flex items-center font-medium text-gray-900 title-font dark:text-gray-300 logoDiv'>
