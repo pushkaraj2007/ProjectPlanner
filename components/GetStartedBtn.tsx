9a31470ab185d52636312766906778a0c49ffd17
Pushkaraj Kulkarni <pushkarajkulkarni07@gmail.com>
2023-09-29 22:45:59 +0530

    ..

--- components/GetStartedBtn.tsx
+++ components/GetStartedBtn.tsx
@@ -6,15 +6,17 @@ import { FiArrowRight } from "react-icons/fi";
 
 const GetStarted = () => {
     const { data: session, status } = useSession();
-    
-    if(status === 'authenticated'){
+
+    if (status === 'authenticated') {
         return (
-            <Link href={'/dashboard'} className="bg-blue-700 hover:bg-blue-600 text-white flex items-center px-6 py-4 mt-6 md:mt-8 text-xl font-semibold rounded-full shadow-lg hover:shadow-xl transform hover:scale-105 transition duration-300">
-                Get Started <FiArrowRight className="ml-2" />
+            <Link href={'/dashboard'}>
+                <button className="bg-blue-700 hover:bg-blue-600 text-white flex items-center px-6 py-4 mt-6 md:mt-8 text-xl font-semibold rounded-full shadow-lg hover:shadow-xl transform hover:scale-105 transition duration-300">
+                    Get Started <FiArrowRight className="ml-2" />
+                </button>
             </Link>
         )
     }
-    else{
+    else {
         return (
             <button className="bg-blue-700 hover:bg-blue-600 text-white flex items-center px-6 py-4 mt-6 md:mt-8 text-xl font-semibold rounded-full shadow-lg hover:shadow-xl transform hover:scale-105 transition duration-300" onClick={() => signIn('google', {
                 callbackUrl: `https://projectplanner.vercel.app/api/user-signin`
