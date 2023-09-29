9dbcbd3e804478a39ec1679bbab0b1d0452f2764
Pushkaraj Kulkarni <107422421+pushkaraj2007@users.noreply.github.com>
2023-09-29 19:31:54 +0530

    Create GetStartedBtn.tsx

---
+++ components/GetStartedBtn.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { signIn, signOut, useSession } from "next-auth/react";
+
+export default function GetStarted() {
+  <button className="bg-blue-700 hover:bg-blue-600 text-white flex items-center px-6 py-4 mt-6 md:mt-8 text-xl font-semibold rounded-full shadow-lg hover:shadow-xl transform hover:scale-105 transition duration-300" signIn('google', {
+                callbackUrl: `https://projectplanner.vercel.app/api/user-signin`
+            })}>
+    Get Started <FiArrowRight className="ml-2" />
+  </button>
+}
